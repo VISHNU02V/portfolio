b80293ec9777a9bf77afe6c2bac45fdb909a67b7
vishhnu02v <vishnuvaivid@gmail.com>
2024-10-30 12:21:08 +0530

    scrolling bar added, moving percentage

--- script.js
+++ script.js
@@ -76,3 +76,12 @@ window.addEventListener('DOMContentLoaded', function() {
         section.style.scrollMarginTop = '8vh';
     });
 });
+
+const scrollProgress = document.getElementById("scroll-progress");
+const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
